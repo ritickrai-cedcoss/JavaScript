9a3ddbb6b84acda3c50a421c6f7b82ba1491cdab
ritick <ritick.official@gmail.com>
2022-02-07 09:35:10 +0530

    understanding uninitialized variables

--- src/script.js
+++ src/script.js
@@ -1,4 +1,9 @@
-var a = 9;
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a ";
+// Only change code above this line
 
-var myFirstName = "cedcoss";
-var myLastName = "simulating"
+a = a + 1;
+b = b + 5;
+c = c + " String!";
